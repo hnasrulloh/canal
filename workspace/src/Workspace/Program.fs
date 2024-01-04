164a5094749c595d4738cdc094b384ae50758ff3
Hilman Nasrulloh <hnasrulloh@hotmail.com>
2024-01-04 15:03:09 +0700

    Setup .NET project from Workspace

---
+++ workspace/src/Workspace/Program.fs
@@ -0,0 +1,11 @@
+﻿open StreamJsonRpc
+open FSharp.Collections.ParallelSeq
+
+let total =
+    [ "1"; "2"; "3" ]
+    |> PSeq.map (fun x -> int x)
+    |> PSeq.map (fun x -> x * 2)
+    |> PSeq.toList
+    |> List.fold (fun acc x -> acc + x) 0
+
+printfn $"Hello {total} times!"
