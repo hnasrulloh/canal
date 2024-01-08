a6b973afd0987a3afe1c2278bdef4cc8bcdc414e
Hilman Nasrulloh <hnasrulloh@hotmail.com>
2024-01-08 09:21:28 +0700

    Setup Primevue

--- editor/src/App.vue
+++ editor/src/App.vue
@@ -1,85 +1,60 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router"
-import HelloWorld from "./components/HelloWorld.vue"
+import { onMounted, ref } from "vue"
+
+onMounted(() => {
+  products.value = [
+    {
+      id: "1000",
+      code: "f230fh0g3",
+      name: "Bamboo Watch",
+      category: "Accessories",
+      quantity: 24
+    },
+    {
+      id: "11111",
+      code: "1h23h12",
+      name: "Clothe",
+      category: "Apparel",
+      quantity: 1
+    },
+    {
+      id: "10982",
+      code: "k12j3h",
+      name: "Nike Shit",
+      category: "Garbage",
+      quantity: 0
+    },
+    {
+      id: "12312",
+      code: "0asd9a9sd",
+      name: "Juice Jinggle",
+      category: "Food",
+      quantity: 0
+    }
+  ]
+})
+
+const products = ref()
+
+const columns = [
+  { field: "code", header: "Code" },
+  { field: "name", header: "Name" },
+  { field: "category", header: "Category" },
+  { field: "quantity", header: "Quantity" }
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>Hi!</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <PrimeDataTable :value="products" tableStyle="min-width: 50rem">
+      <PrimeColumn
+        v-for="col of columns"
+        :key="col.field"
+        :field="col.field"
+        :header="col.header"
+      ></PrimeColumn>
+    </PrimeDataTable>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- editor/src/main.ts
+++ editor/src/main.ts
@@ -1,7 +1,12 @@
-import "./assets/main.css"
+import "primevue/resources/themes/viva-light/theme.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
 
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import PrimeVue from "primevue/config"
+import DataTable from "primevue/datatable"
+import Column from "primevue/column"
 
 import App from "./App.vue"
 import router from "./router"
@@ -10,5 +15,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+
+app.component("PrimeDataTable", DataTable)
+app.component("PrimeColumn", Column)
 
 app.mount("#app")
