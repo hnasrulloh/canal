1b261b4733d181397f7a54c6417329ce070fd1b3
Hilman Nasrulloh <hnasrulloh@hotmail.com>
2024-06-02 18:13:11 +0700

    Use child instead of pid

--- crates/kernel/src/repl.rs
+++ crates/kernel/src/repl.rs
@@ -1,19 +1,22 @@
-use std::panic::{self, UnwindSafe};
+use std::{
+    panic::{self, UnwindSafe},
+    process,
+};
 use thiserror::Error;
 
 #[derive(Debug)]
 pub struct Repl {
-    pid: u32,
+    process: process::Child,
 }
 
 impl Repl {
-    pub async fn launch<F>(spawn_repl: F) -> Result<Self, ReplError>
+    pub fn launch<F>(spawn_repl: F) -> Result<Self, ReplError>
     where
-        F: Fn() -> u32 + 'static + UnwindSafe,
+        F: Fn() -> process::Child + 'static + UnwindSafe,
     {
         let catch_unwind = panic::catch_unwind(spawn_repl);
         match catch_unwind {
-            Ok(pid) => Ok(Self { pid }),
+            Ok(child) => Ok(Self { process: child }),
             Err(_) => Err(ReplError::SpawnFailed),
         }
     }
@@ -29,27 +32,4 @@ pub enum ReplError {
 mod tests {
     use super::*;
     use googletest::prelude::*;
-
-    #[googletest::test]
-    #[tokio::test]
-    async fn starting_a_repl_returns_ok() {
-        let result = Repl::launch(try_spawn_repl_then_success).await;
-        expect_that!(result, pat!(Ok(_)));
-    }
-
-    fn try_spawn_repl_then_success() -> u32 {
-        let pid = 123;
-        pid
-    }
-
-    #[googletest::test]
-    #[tokio::test]
-    async fn starting_a_repl_returns_error() {
-        let result = Repl::launch(try_spawn_repl_then_fail).await;
-        expect_that!(result, pat!(Err(pat!(ReplError::SpawnFailed))));
-    }
-
-    fn try_spawn_repl_then_fail() -> u32 {
-        panic!("Failed to run a new REPL process");
-    }
 }
---
+++ crates/kernel/tests/repl.rs
@@ -0,0 +1,1 @@
+// TODO: Test repl launch (see https://stackoverflow.com/questions/77120851/rust-mocking-stdprocesschild-for-test)
